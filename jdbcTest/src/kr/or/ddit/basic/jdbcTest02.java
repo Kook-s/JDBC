71c3746afc57badd697477c91bf72b38bf3b512b
PC-20 <PC-20@DESKTOP-1J4B3U9>
2023-03-14 12:26:43 +0900

    JDCB

--- jdbcTest/src/kr/or/ddit/basic/jdbcTest02.java
+++ jdbcTest/src/kr/or/ddit/basic/jdbcTest02.java
@@ -7,41 +7,34 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
+import oracle.jdbc.dcn.DatabaseChangeEvent;
+
 public class jdbcTest02 {
 	public static void main(String[] args) {
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
 
+		Scanner scan = new Scanner(System.in);
+		System.out.print("숫자 입력 >> ");
+		int num = scan.nextInt();
+		System.out.println(num + "값 보다 큰자료 출력 합니다\n");
+
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-
 			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SHIN", "java");
 
-			String sql = "SELECT * FROM lprod";
-
+			String sql = "SELECT * FROM lprod where lprod_id > " + num;
 			stmt = conn.createStatement();
-
-		
 			rs = stmt.executeQuery(sql);
 
-			
 			System.out.println(" === SQL문 처리 결과 ===");
-
-			
-			Scanner scan = new Scanner(System.in);
-			System.out.print("숫자 입력 >> ");
-			int num =scan.nextInt();
-			System.out.println(num+"값 보다 큰자료 출력 합니다\n");
 			while (rs.next()) {
-				// ResultSet객체의 포인터가 가르키는 곳의 데이터를 가져오는 방법
-				// 형식1) re.get자료형 이름("컬럼명 또는 alias명")
-				// 형식2) re.get자료형 이름(컬럼번호) >> 컬럼번호는 1부터 시작
-				if(rs.getInt("lprod_id")>num) {
+
 				System.out.println("Lprod_id : " + rs.getInt("lprod_id"));
 				System.out.println("Lprod_gu : " + rs.getString(2));
 				System.out.println("Lprod_nm : " + rs.getString("lprod_nm"));
-				System.out.println("--------------------------------------");}
+				System.out.println("--------------------------------------");
 			}
 
 		} catch (ClassNotFoundException e) {
