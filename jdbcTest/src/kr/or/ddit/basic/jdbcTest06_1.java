45b0631d061efd5efa29c5c44051e8a10f8e5ee1
PC-20 <PC-20@DESKTOP-1J4B3U9>
2023-03-16 12:38:46 +0900

    JDBC

--- jdbcTest/src/kr/or/ddit/basic/jdbcTest06_1.java
+++ jdbcTest/src/kr/or/ddit/basic/jdbcTest06_1.java
@@ -3,11 +3,13 @@ package kr.or.ddit.basic;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import kr.or.ddit.basic.utill.DBUtil;
 
-public class jdbcTest06_1 { //jdbcTest06 더 정리한거
+public class jdbcTest06_1 { // jdbcTest06 더 정리한거
 	private Connection conn = null;
 	private PreparedStatement pstmt = null;
 	private ResultSet rs = null;
@@ -60,7 +62,10 @@ public class jdbcTest06_1 { //jdbcTest06 더 정리한거
 				displayAllMenu();
 				break;
 			case 5: // 전체출력
-				displayAllMenu2();
+				updateMember2();
+				break;
+			case 6: // 전체출력
+				updateMember3();
 				break;
 
 			case 0:
@@ -74,7 +79,96 @@ public class jdbcTest06_1 { //jdbcTest06 더 정리한거
 
 	}
 
-	private void displayAllMenu2() {
+	private void updateMember3() {
+		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println("수정할 회원 정보를 입력하세요");
+		System.out.print("회원ID >> ");
+		String id = scan.next();
+
+		int count = getMemberCount(id);
+		if (count == 0) {
+			System.out.println(id + "는(은) 없는 회원ID 입니다...");
+			System.out.println("수정 작업을 마칩니다...");
+			return;
+		}
+		// key값은 : 수정할 컬럼명, value값 : 수정할 데이터 값
+		// 수정할 데이터값이 있을 때만 Map에 추가된다.
+		Map<String, String> dataMap = new HashMap<>();
+
+		System.out.println();
+		scan.nextLine();
+		
+		System.out.print("새로운 비밀번호 >> ");
+		String newPass = scan.nextLine().trim();
+		if (!"".equals(newPass)) {
+			dataMap.put("mem_pass", newPass);
+		}
+
+		System.out.print("새로운 회원이름>> ");
+		String newName = scan.nextLine().trim();
+		if (!"".equals(newName)) {
+			dataMap.put("mem_name", newName);
+		}
+
+		System.out.print("새로운 전화번호 >> ");
+		String newTel = scan.nextLine().trim();
+		if (!"".equals(newTel)) {
+			dataMap.put("mem_tel", newTel);
+		}
+
+		System.out.print("새로운 회원주소 >> ");
+		String newAddr = scan.nextLine().trim();
+		if (!"".equals(newAddr)) {
+			dataMap.put("mem_addr", newAddr);
+		}
+		
+		try {
+			conn = DBUtil.getConnection();
+
+			String temp = ""; // SQL문의 set이후에 수정할 컬럼 설정하는 부누이 저장될 변수
+			// ? 안사용할때 
+//			for (String fieldName : dataMap.keySet()) {
+//				if (!"".equals(temp)) {
+//					temp += ", ";
+//				}
+//				temp += fieldName + " = " + "'" + dataMap.get(fieldName) + "' ";
+//			}
+//			
+//			String sql = "update mymember set " + temp + " where mem_id = ?";
+//			pstmt = conn.prepareStatement(sql);
+//			pstmt.setString(1, id);
+
+			// ?사용일때 
+			for (String fieldName : dataMap.keySet()) {
+				if (!"".equals(temp)) {
+					temp += ", ";
+				}
+				temp += fieldName + " = ? ";
+			}
+
+			String sql = "update mymember set " + temp + " where mem_id = ?";
+			pstmt = conn.prepareStatement(sql);
+
+			int num = 1;
+			for (String fieldName : dataMap.keySet()) {
+				pstmt.setString(num++, dataMap.get(fieldName));
+			}
+			pstmt.setString(num, id);
+
+			int cnt = pstmt.executeUpdate();
+
+			if (cnt > 0) {
+				System.out.println("회원 ID가 " + id + "인 회원정보 수정 성공!!!");
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+	private void updateMember2() {
 		// TODO Auto-generated method stub
 		System.out.println();
 		System.out.println("수정할 회원 정보를 입력하세요");
@@ -97,7 +191,7 @@ public class jdbcTest06_1 { //jdbcTest06 더 정리한거
 		System.out.print("선택 >>");
 		String sql = "update mymember set ";
 		String input = "";
-		
+
 		switch (scan.nextInt()) {
 		case 1:
 			System.out.print("새로운 비밀번호 >> ");
@@ -122,11 +216,11 @@ public class jdbcTest06_1 { //jdbcTest06 더 정리한거
 			break;
 		}
 		try {
-			sql+= " where mem_id = ?";
+			sql += " where mem_id = ?";
 			scan.nextLine();
 			input = scan.nextLine();
-			
-			conn= DBUtil.getConnection();
+
+			conn = DBUtil.getConnection();
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, input);
 			pstmt.setString(2, id);
@@ -326,9 +420,10 @@ public class jdbcTest06_1 { //jdbcTest06 더 정리한거
 		System.out.println("--------------------");
 		System.out.println(" 1. 자료 추가 ");
 		System.out.println(" 2. 자료 삭제 ");
-		System.out.println(" 3. 자료 수정 ");
+		System.out.println(" 3. 자료 수정 (전체항목 수정");
 		System.out.println(" 4. 전체 자료 출력");
-		System.out.println(" 5. 자료 수정2 ");
+		System.out.println(" 5. 자료 수정2 (수정항목 선택)");
+		System.out.println(" 6. 자료 수정3 (입력항목만 수정)");
 		System.out.println(" 0. 작업 끝.");
 		System.out.println("--------------------");
 		System.out.print(" 선택하세요 >>");
